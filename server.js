04a5e63c493d1531c1a6b8a0f72acd76f59b1a3a
Jason <haloos@me.com>
2019-08-20 13:02:00 -0400

    added middleware

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express(); 
 
+app.use((req, res, next) => {
+  console.log('<h1>HELLOS</h1>') 
+  next();
+})
+
 app.get('/', (req, res) => {
   res.send("helllooooo"); 
 })
