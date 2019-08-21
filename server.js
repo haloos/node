9cc1de03cebce3732d62599386184c5abeecd243
Jason Paredes <haloos@me.com>
2019-08-21 18:26:31 -0400

    added bodyParser

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express'); 
+const bodyParser = require('body-parser'); 
 
 const app = express(); 
 
-app.use((req, res, next) => {
-  console.log('<h1>HELLOS</h1>') 
-  next();
-})
-
+app.use(bodyParser.urlencoded({extended: false})) 
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
-  res.send("helllooooo"); 
-})
+  res.send("getting root"); 
+});
+
+app.get('/profile', (req, res) => {
+  res.send("getting profile"); 
+}); 
+
+app.post('/profile', (req, res) => { 
+  console.log(req.body)
+  const user = {
+    name: 'Sally', 
+    hobby: 'soccer'
+  } 
+  res.send(user); 
+});
 
 app.listen(3000);
